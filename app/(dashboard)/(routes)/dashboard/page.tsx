6870112b2bdc2201ace44ec76b0595791018ae77
anandkr47 <anandkumar.trident@gmail.com>
2023-11-13 16:37:55 +0530

    added favicon.ico

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Button } from "@/components/ui/button"
+import { UserButton } from '@clerk/nextjs'
 
 
 export default function Dashboard() {
   return (
     <div>
       <h1 className='text-blue-500 text-6xl'>Dashboard</h1>
-      <Button>Click me</Button>
+      <UserButton afterSignOutUrl='/' />
       <Button variant="outline">Button</Button>
       <Button variant="ghost">Button</Button>
       </div>
