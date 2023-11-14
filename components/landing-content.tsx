7172e8790b8f8f6aba334da018d9af8d64ce9ee6
anandkr47 <anandkumar.trident@gmail.com>
2023-11-14 18:31:11 +0530

    lading page updted

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
 
 const testimonials = [
   {
@@ -32,7 +33,9 @@ const testimonials = [
 export const LandingContent = () => {
   return (
     <div className="px-10 pb-20">
+        
       <h2 className="text-center text-4xl text-white font-extrabold mb-10">Testimonials</h2>
+      
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {testimonials.map((item) => (
           <Card key={item.description} className="bg-[#192339] border-none text-white">
--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -11,7 +11,8 @@ export const LandingHero = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <div className="text-white font-bold py-36 text-center space-y-5">
+    <div className="text-white font-bold py-20 text-center space-y-5">
+        
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Best AI Tool for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-white">
@@ -45,6 +46,9 @@ export const LandingHero = () => {
       </div>
       <div className="text-zinc-400 text-xs md:text-sm font-normal">
         No credit card required.
+      </div>
+      <div className="flex justify-center items-center my-4 mx-auto">
+        <Image alt="Your GIF Description" src="/bird.gif" width={200} height={200} />
       </div>
     </div>
   );
