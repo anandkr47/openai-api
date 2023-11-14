fe61effe0b99d2f5ee85437b3d8a56cb80917d25
anandkr47 <anandkumar.trident@gmail.com>
2023-11-14 14:33:14 +0530

    landing page layout update

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import TypewriterComponent from "typewriter-effect";
+import Image from "next/image";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
 
@@ -13,7 +14,7 @@ export const LandingHero = () => {
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Best AI Tool for</h1>
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-white">
           <TypewriterComponent
             options={{
               strings: [
@@ -27,6 +28,10 @@ export const LandingHero = () => {
             }}
           />
         </div>
+        
+      </div>
+      <div className="flex justify-center items-center my-4 mx-auto">
+        <Image alt="Your GIF Description" src="/robo.gif" width={200} height={200} />
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
         Create content using AI 10x faster.
