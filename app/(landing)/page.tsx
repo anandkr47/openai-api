6da607c4ea59a115c6b08a15f9d7f47cddb53632
anandkr47 <anandkumar.trident@gmail.com>
2023-11-14 12:29:59 +0530

    landing page card content added

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,24 +1,15 @@
-import Image from 'next/image'
-import { Button } from "@/components/ui/button"
-import Link from 'next/link'
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
 
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className='text-blue-500 text-6xl'>OpenAI</h1>
-      <Button>Click me</Button>
-      <Button variant="outline">Button</Button>
-      <Button variant="ghost">Button</Button>
-      <div>
-          <Link href='/sign-in'>
-          <Button>Sign in</Button>
-          </Link>
-          <Link href='/sign-up'>
-          <Button>Sign up</Button>
-          </Link>
-      </div>
-      
-      </div>
-      )
+const LandingPage = () => {
+  return ( 
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+    </div>
+   );
 }
+ 
+export default LandingPage;
