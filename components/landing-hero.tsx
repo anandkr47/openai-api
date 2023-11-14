e8094d20f748ab896b816a2172902e67155c51aa
anandkr47 <anandkumar.trident@gmail.com>
2023-11-14 20:56:55 +0530

    bid annimation

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,18 +1,53 @@
-"use client";
-
+// components/LandingHero.tsx
+'use client'
+import React, { useRef, useEffect, useState } from "react";
 import TypewriterComponent from "typewriter-effect";
 import Image from "next/image";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
-
 import { Button } from "@/components/ui/button";
 
-export const LandingHero = () => {
+export const LandingHero: React.FC = () => {
   const { isSignedIn } = useAuth();
+  const birdRef = useRef<HTMLImageElement>(null);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [rotationAngle, setRotationAngle] = useState(0);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+
+      if (birdRef.current) {
+        const bird = birdRef.current;
+        const boundingBox = bird.getBoundingClientRect();
+        const dx = e.clientX - (boundingBox.left + boundingBox.width / 2);
+        const dy = e.clientY - (boundingBox.top + boundingBox.height / 2);
+        const angle = (Math.atan2(dy, dx) * 180) / Math.PI;
+        setRotationAngle(angle);
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const birdStyle: React.CSSProperties = {
+    position: "fixed",
+    top: `${mousePosition.y}px`,
+    left: `${mousePosition.x}px`,
+    transform: `translate(-10%, -10%) rotate(${rotationAngle}deg)`,
+    transition: "transform 0.2s ease-in-out",
+  };
+
+  const hideCursorStyle: React.CSSProperties = {
+    cursor: "none",
+  };
 
   return (
-    <div className="text-white font-bold py-20 text-center space-y-5">
-        
+    <div className="text-white font-bold py-20 text-center space-y-5" style={hideCursorStyle}>
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
         <h1>The Best AI Tool for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-white">
@@ -22,14 +57,13 @@ export const LandingHero = () => {
                 "Chatbot.",
                 "Photo Generation.",
                 "Blog Writing.",
-                "Mail Writing."
+                "Mail Writing.",
               ],
               autoStart: true,
               loop: true,
             }}
           />
         </div>
-        
       </div>
       <div className="flex justify-center items-center my-4 mx-auto">
         <Image alt="Your GIF Description" src="/robo.gif" width={200} height={200} />
@@ -47,8 +81,8 @@ export const LandingHero = () => {
       <div className="text-zinc-400 text-xs md:text-sm font-normal">
         No credit card required.
       </div>
-      <div className="flex justify-center items-center my-4 mx-auto">
-        <Image alt="Your GIF Description" src="/bird.gif" width={200} height={200} />
+      <div style={birdStyle}>
+        <Image alt="bird-gif" src="/bird.gif" width={150} height={200} ref={birdRef} />
       </div>
     </div>
   );
